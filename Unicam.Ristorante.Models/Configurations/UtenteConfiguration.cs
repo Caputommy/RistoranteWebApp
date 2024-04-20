df43a4360a950c6726d8a70bdfc02dbe79bd3ee3
Caputommy <caputommy@gmail.com>
2024-04-20 19:26:05 +0200

    Aggiunte prime classi per EntityFramework

---
+++ Unicam.Ristorante.Models/Configurations/UtenteConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Unicam.Ristorante.Models.Entities;
+
+namespace Unicam.Ristorante.Models.Configurations
+{
+    internal class UtenteConfiguration : IEntityTypeConfiguration<Utente>
+    {
+
+        public void Configure(EntityTypeBuilder<Utente> builder)
+        {
+            builder.ToTable("Utenti");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Email).HasMaxLength(100).IsRequired();
+            builder.Property(u => u.Nome).HasMaxLength(100);
+            builder.Property(u => u.Cognome).HasMaxLength(100);
+            builder.Property(u => u.Password).HasMaxLength(100).IsRequired();
+            builder.Property(u => u.Ruolo).IsRequired();
+        }
+
+    }
+}
---
+++ Unicam.Ristorante.Models/Context/MyDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Unicam.Ristorante.Models.Context
+{
+    public class MyDbContext : DbContext
+    {
+
+        public MyDbContext() : base()
+        {
+
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> config) : base(config)
+        {
+
+        }
+
+        //public DbSet<Azienda> Aziende { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+               .UseSqlServer("data source=localhost;Initial catalog=paradigmi;User Id=paradigmi;Password=paradigmi;TrustServerCertificate=True");
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Azienda>(new AziendaConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
